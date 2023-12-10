78b92c92a475bc2d5e32b56ca2ac66141bdaa97c
K0brAAA <148149595+K0brAAA@users.noreply.github.com>
2023-12-10 15:09:53 +0200

    add_base

--- milionerzy.py
+++ milionerzy.py
@@ -1,73 +1,30 @@
 import json
-import random
-JSON_FILE_USERDATABASE = "MyUsers.json"
-# Klasa reprezentująca pytanie
-class Question:
-    def __init__(self, text, options, correct_option):
-        self.text = text
-        self.options = options
-        self.correct_option = correct_option
+import question
+JSON_FILE_USERS_DATABASE = "MyUsers.json"
 
-# Lista pytań
-questions = [
-    Question("Jaki jest stolica Francji?", ["A. Berlin", "B. Madryt", "C. Paryż", "D. Londyn"], "C"),
-    Question("Który gaz jest najbardziej obfity w atmosferze Ziemi?", ["A. Azot", "B. Tlen", "C. Wodór", "D. Hel"], "A"),
-    Question("Ile wynosi 2 do potęgi 5?", ["A. 16", "B. 32", "C. 64", "D. 128"], "B"),
-    # Dodaj więcej pytań według potrzeб
-]
 
-# Funkcja losująca pytanie
-def select_random_question(questions):
-    return random.choice(questions)
+def users_database_initialization():
+    users_base = {"users_dane": []}
+    with open(JSON_FILE_USERS_DATABASE, "w") as file:
+        json.dump(users_base, file)
 
-# Funkcja prezentująca pytanie i opcje odpowiedzi
-def present_question(question):
-    print(question.text)
-    for option in question.options:
-        print(option)
-    user_answer = input("Wybierz odpowiedź (np. 'A'): ").upper()
-    return user_answer
 
-# Funkcja sprawdzająca, czy odpowiedź jest poprawна
-def check_answer(question, user_answer):
-    return user_answer == question.correct_option
-
-# Funkcja główna gry
-def play_millionaire(questions):
-    score = 0
-    for _ in range(15):  # Gracz ma 15 pytań do odpowiedzenia
-        question = select_random_question(questions)
-        user_answer = present_question(question)
-        if check_answer(question, user_answer):
-            score += 1
-            print("Odpowiedź poprawna!\n")
-        else:
-            print("Niestety, odpowiedź nieprawidłowa. Koniec gry.")
-            break
-    if score == 15:
-        print("Gratulacje! Wygrałeś milion złotych!")
-    else:
-        print("Zdobyłeś {} złotych. Dziękujemy za udział w grze.".format(1000 * score))
-
-def inicjalizuj_baze_danych():
-    UsersBase = {"users_dane": []}
-    with open(JSON_FILE_USERDATABASE, "w") as file:
-        json.dump(UsersBase, file)
-
-def zapisz_produkty(users):
+def user_record(users):
     dane_of_users = {"users_dane": users}
-    with open(JSON_FILE_USERDATABASE, "w") as file:
+    with open(JSON_FILE_USERS_DATABASE, "w") as file:
         json.dump(dane_of_users, file, indent=3)
 
-def add_new_user(User_name, Password, Mail):
-    MyUser = load_user()
+
+def add_new_user(username, password, email):
+    my_user = load_user()
     new_users = {
-        "Username": User_name,
-        "Password": Password,
-        "E-Mail": Mail
+        "username": username,
+        "password": password,
+        "email": email
     }
-    MyUser.append(new_users)
-    zapisz_produkty(MyUser)
+    my_user.append(new_users)
+    user_record(my_user)
+
 
 def load_user():
     try:
@@ -75,33 +32,37 @@ def load_user():
             dane = json.load(file)
             return dane.get("users_dane", [])
     except (FileNotFoundError, json.JSONDecodeError):
-        inicjalizuj_baze_danych()
+        users_database_initialization()
         return []
+
 
 def authorize_user(mail, password):
     users = load_user()
     for user in users:
-        if user['E-Mail'] == mail and user['Password'] == password:
+        if user['email'] == mail and user['password'] == password:
             return True
     return False
 
-def wybor():
+
+def choice_in_first_menu():
     print("You have account?")
     print("1. Yes, I have account")
     print("2. No, I don't have account")
-    get_user_input = int(input("Your answer: "))
+    print("3. Exit")
     while True:
         try:
-            if get_user_input in [1, 2]:
+            get_user_input = int(input("Your answer (1, 2, or 3): "))
+            if get_user_input in [1, 2, 3]:
                 return get_user_input
             else:
-                print("Error answer!")
-                get_user_input = int(input("Your answer: "))
+                print("Error: Please enter 1, 2, or 3.")
         except ValueError:
-            pass
-def wybor_1(wybor1):
+            print("Error: Please enter a valid integer.")
+
+
+def selection_authorization(choice1):
     while True:
-        if wybor1 == 1:
+        if choice1 == 1:
             mail = input('Enter your email address: ').strip()
             password = input('Enter your password: ')
             if authorize_user(mail, password):
@@ -109,67 +70,107 @@ def wybor_1(wybor1):
                 break
             else:
                 print('\n| Invalid email address or password |\n')
-def correct_username(User_name):
+
+
+def correct_username(username):
     while True:
         try:
             users = load_user()
             username_taken = False
             for user in users:
-                if user['Username'] == User_name:
+                if user['username'] == username:
                     print("| Username taken. Write a different username |")
-                    User_name = input("Enter your new username: ")
+                    username = input("Enter your new username: ")
                     username_taken = True
                     break
             if not username_taken:
-                CorrectUsername = User_name
-                return CorrectUsername
+                correct_username_in_def = username
+                return correct_username_in_def
         except ValueError:
-            print("Error. Please try again.")
-def correct_password(Password):
+            print(" Error. Please try again.")
+
+
+def correct_password(password):
     while True:
         try:
-            if len(Password) >= 8:
-                correct_password_in_base = Password
+            if len(password) >= 8:
+                correct_password_in_base = password
                 return correct_password_in_base
             else:
-                print("\n| Password is so small, try again. Minmum 8 characters |\n")
-                Password = str(input("Your password: "))
+                print("\n| Password is so small, try again. Minimum 8 characters |\n")
+                password = str(input("Your password: "))
         except ValueError:
             print("|Error. Try again.|\n")
-def wybor_2(wybor2):
-    if wybor2 == 2:
-            User_name = str(input("What is your username?: ")).strip()
-            Password = str(input("Your password: "))
-            Mail = str(input("Your e-mail: ")).strip()
-            CorrectUsername = correct_username(User_name)
-            CorrectPassword = correct_password(Password)
 
-            while True:
-                try:
-                    if Mail.count('@') == 1 \
-                            and Mail.count('.') > 0 \
-                            and Mail[0] != '@' \
-                            and Mail.rfind('.') > Mail.rfind('@'):
-                        CorrectMail = Mail
-                        break
-                    else:
-                        print('| Invalid e-mail |\n')
-                        Mail = str(input("Your e-mail: ")).strip()
-                except ValueError:
-                    print("\n|Error|\n")
-            add_new_user(CorrectUsername, CorrectPassword, CorrectMail)
-            print("\n!Added new user!")
+
+def selection_registration(choice2):
+    if choice2 == 2:
+        username = str(input("What is your username?: ")).strip()
+        password = str(input("Your password: "))
+        email = str(input("Your e-mail: ")).strip()
+        correct_username_variable = correct_username(username)
+        correct_password_variable = correct_password(password)
+
+        while True:
+            try:
+                if email.count('@') == 1 \
+                        and email.count('.') > 0 \
+                        and email[0] != '@' \
+                        and email.rfind('.') > email.rfind('@'):
+                    correct_email_variable = email
+                    break
+                else:
+                    print('| Invalid e-mail |\n')
+                    email = str(input("Your e-mail: ")).strip()
+            except ValueError:
+                print("\n|Error|\n")
+        add_new_user(correct_username_variable, correct_password_variable, correct_email_variable)
+        print("\n!Added new user!")
+
 
 def first_user_selection_menu():
-    wybor1 = wybor()
-    if wybor1 == 1:
-        wybor_1(wybor1)
-    elif wybor1 == 2:
-        wybor_2(wybor1)
+    choice = choice_in_first_menu()
+    if choice == 1:
+        selection_authorization(choice)
+    elif choice == 2:
+        selection_registration(choice)
+    elif choice == 3:
+        print("Ok, goodbye!")
+        exit()
+
+
+def present_game_menu():
+    print("1. Lets go Play")
+    print("2. Quit")
+    while True:
+        try:
+            get_user_input = int(input("Your answer: "))
+            if get_user_input in [1, 2]:
+                return get_user_input
+            else:
+                print("Error answer!")
+        except ValueError:
+            print("Error. Please try again.")
+
+
+def game_menu():
+    while True:
+        try:
+            choice_in_game_menu = present_game_menu()
+            if choice_in_game_menu == 1:
+                question.play_millionaire()
+            else:
+                print("Ok, goodbye!")
+                exit()
+        except ValueError:
+            print("Error. Please try again.")
 
 
 if __name__ == "__main__":
-    print('''\n!!!Welcome to the game "Millionaire"!!!\n
-   To continue, register or log in! 
-   Please select the correct option!\n''')
+    my_list = \
+        ["Welcome to the game 'Millionaire'!!!", "To continue, register or log in!",
+            "Please select the correct option!\n"]
+    for _ in range(3):
+        print(my_list[_])
     first_user_selection_menu()
+    game_menu()
